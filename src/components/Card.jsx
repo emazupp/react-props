5742359feb0bb08c633cd273985fbae324cfd600
emazupp <zuppardo.emanuele2001@gmail.com>
2024-11-29 15:50:52 +0100

    added props on card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,14 @@
 import Button from "./Button/Button";
 
-export default function Card({ title, image, desciption, tags }) {
+export default function Card({ title, image, description, tags }) {
   return (
     <div className="card">
       <div className="card-img">
-        <img src="https://picsum.photos/600/400" alt="img" />
+        <img src={image || "https://picsum.photos/600/400"} alt="img" />
       </div>
       <div className="card-body">
-        <h3>Titolo del post</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-          explicabo illo commodi assumenda quos quas dolores quod fuga. Soluta
-          quos obcaecati labore iusto neque recusandae fugiat eaque debitis
-          praesentium veniam?
-        </p>
+        <h3>{title}</h3>
+        <p>{description}</p>
         <Button />
       </div>
     </div>
