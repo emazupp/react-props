452ebfd035cb003b359fed6fddf58048fdbc1328
emazupp <zuppardo.emanuele2001@gmail.com>
2024-11-29 15:56:03 +0100

    add condition props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,15 @@
 import Button from "./Button/Button";
 
 export default function Card({ title, image, description, tags }) {
+  const defaultImage = "https://picsum.photos/600/400";
   return (
     <div className="card">
       <div className="card-img">
-        <img src={image || "https://picsum.photos/600/400"} alt="img" />
+        <img src={image || defaultImage} alt="img" />
       </div>
       <div className="card-body">
-        <h3>{title}</h3>
-        <p>{description}</p>
+        {title && <h3>{title}</h3>}
+        {description && <p>{description}</p>}
         <Button />
       </div>
     </div>
